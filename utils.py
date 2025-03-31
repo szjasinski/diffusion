67bf04dba47576de9b01722e746c01cd2d1186f0
Szymon Jasinski <szjasinski@outlook.com>
2025-03-31 21:15:30 +0200

    update data loading

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 import torch
 import torchvision
 import torchvision.transforms as transforms
-from torch.utils.data import DataLoader, Subset
+from torch.utils.data import DataLoader, Dataset
 
 import matplotlib.pyplot as plt
 
@@ -11,44 +11,36 @@ from datetime import datetime
 from typing import Tuple, Sequence
 
 
-def load_transformed_CIFAR10_subset(batch_size: int, 
-                                    img_size: int = 32,
-                                    train: bool = True,
-                                    label: int = 1,
-                                    path: str  = "./data/") -> Tuple[Subset, DataLoader]:
+def load_transformed_CIFAR10(batch_size: int, 
+                            img_size: int = 32,
+                            train: bool = True,
+                            path: str  = "./data/") -> Tuple[Dataset, DataLoader]:
     
     """
-    Loads a subset of the CIFAR-10 dataset containing only images of a specific class label, 
-    applies transformations, and returns both a dataset subset and a DataLoader.
+    Loads transformed CIFAR-10.
 
     Args:
         batch_size (int): The number of samples per batch in the DataLoader.
         img_size (int, optional): The target size to resize images to (height, width). Defaults to 32.
         train (bool, optional): Whether to load the training set (True) or test set (False). Defaults to True.
-        label (int, optional): The class label to filter (e.g., 1 for automobiles). Defaults to 1.
         path (str, optional): The root directory for downloading/storing the dataset. Defaults to "./data/".
 
     Returns:
-        Tuple[Subset, DataLoader]: 
-            - A `Subset` of CIFAR-10 containing only images of the specified class label.
-            - A `DataLoader` for iterating over the subset.
+        Tuple[Dataset, DataLoader]: 
     """
     
     transformation_list = [
         transforms.Resize((img_size, img_size)),  # Resize to specified img_size
+        transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),  # Convert to tensor and scale to [0, 1]
         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),  # Normalize to [-1, 1]
     ]
 
     data_transform = transforms.Compose(transformation_list)
     dataset = torchvision.datasets.CIFAR10(path, download=True, train=train, transform=data_transform)
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, drop_last=True)
 
-    subset_indices = [idx for idx, lbl in enumerate(dataset.targets) if lbl == label]
-    subset = Subset(dataset, subset_indices)
-
-    dataloader = DataLoader(subset, batch_size=batch_size, shuffle=True, drop_last=True)
-
-    return subset, dataloader
+    return dataset, dataloader
 
 
 def visualize_images(images: Sequence[torch.Tensor], 
